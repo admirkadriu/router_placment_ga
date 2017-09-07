3f26cc29bfbbcad422a850e98a9251feebbd0508
Admir Kadriu <admir.kadriu@solaborate.com>
2017-09-08 00:56:39 +0200

    solution refactor

--- models/solution.py
+++ models/solution.py
@@ -2,6 +2,7 @@ from uuid import uuid4
 
 from models.building import Building
 from models.router import Router
+from models.cell import Cell
 
 
 class Solution:
@@ -24,7 +25,7 @@ class Solution:
                 routers.append(Router.at_random_target())
 
             i += 1
-
+        print(Cell.calculation_duration/Cell.calculation_count, Cell.no_calculation_duration/Cell.no_calculation_count)
         return solution
 
     def __init__(self):
@@ -34,23 +35,15 @@ class Solution:
         self.score = None
         self.calculateScore = True
 
-    def get_score(self):
-        if self.calculateScore:
-            self.calc_score()
-        return self.score
-
     def connected_cells_count(self):
         return len(self.connected_cells)
 
     def routers_count(self):
         return len(self.routers)
 
-    def calc_score(self):
-        covered_cells_score = 1000 * len(Building.get_covered_cells(self.routers))
-        remaining_budget_score = Building.infrastructure_budget - (
-            self.connected_cells_count() * Building.back_bone_cost + self.routers_count() * Router.unit_cost)
-        self.score = covered_cells_score + remaining_budget_score
-        self.calculateScore = False
+    def get_score(self):
+        if self.calculateScore:
+            self.calc_score()
         return self.score
 
     def get_cost(self):
@@ -59,16 +52,16 @@ class Solution:
     def is_feasible(self):
         return self.get_cost() <= Building.infrastructure_budget
 
-    def connect_routers(self):
-        self.connected_cells = []
-        self.routers.sort(key=lambda r: r.cell.get_distance_to_backbone())
+    def calc_score(self):
+        budget_for_routers = self.routers_count() * Router.unit_cost
+        budget_to_connect_routers = self.connected_cells_count() * Building.back_bone_cost
 
-        for router in self.routers:
-            cells_to_connect = router.get_best_path(self.connected_cells)
-            self.connected_cells += cells_to_connect
+        remaining_budget_score = Building.infrastructure_budget - budget_for_routers - budget_to_connect_routers
+        covered_cells_score = 1000 * len(Building.get_covered_cells(self.routers))
 
-        self.calculateScore = True
-        return
+        self.score = covered_cells_score + remaining_budget_score
+        self.calculateScore = False
+        return self.score
 
     def add_router(self, router):
         cells_to_connect = router.get_best_path(self.connected_cells)
@@ -81,3 +74,16 @@ class Solution:
             return True
 
         return False
+
+    def reconnect_routers(self):
+        self.connected_cells = []
+        self.routers.sort(key=lambda r: r.cell.get_distance_to_backbone())
+
+        for router in self.routers:
+            cells_to_connect = router.get_best_path(self.connected_cells)
+            self.connected_cells += cells_to_connect
+
+        self.calculateScore = True
+        return
+
+
