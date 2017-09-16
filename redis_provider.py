bb5782fd137b3ce82d323cbf69aa8cac6e0ca7fe
Admir Kadriu <admir.kadriu@solaborate.com>
2017-09-16 22:32:21 +0200

    redis key and bug on no covered cells

--- redis_provider.py
+++ redis_provider.py
@@ -6,15 +6,16 @@ from models.cell import Cell
 
 class RedisProvider:
     client = redis.StrictRedis(host=Config.redis_host, port=Config.redis_port, db=Config.redis_db)
+    key = Config.file_path + ":"
 
     @staticmethod
     def set(cell_id, value):
-        RedisProvider.client.set(Config.file_path + ":" + cell_id, RedisProvider.to_redis_cells(value))
+        RedisProvider.client.set(RedisProvider.key + cell_id, RedisProvider.to_redis_cells(value))
         return
 
     @staticmethod
     def get(cell_id):
-        ids = RedisProvider.client.get(Config.file_path + ":" + cell_id)
+        ids = RedisProvider.client.get(RedisProvider.key + cell_id)
         if ids is None:
             return None
 
@@ -30,6 +31,9 @@ class RedisProvider:
         ids = ids.decode("utf-8")
         cell_ids = ids.split(";")
         cells = []
+        if len(ids) < 3:
+            return cells
+
         for cell_id in cell_ids:
             cell_id_split = cell_id.split(",")
             cell = Cell.get(int(cell_id_split[0]), int(cell_id_split[1]))
