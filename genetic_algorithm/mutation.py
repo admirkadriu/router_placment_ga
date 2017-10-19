96509ee6e67c9ed7195a4cbac8053e2881ce514c
Admir Kadriu <admir.kadriu@solaborate.com>
2017-10-20 00:38:45 +0200

    2 tries at mutation

--- genetic_algorithm/mutation.py
+++ genetic_algorithm/mutation.py
@@ -18,19 +18,19 @@ class Mutation:
         self.clone.add_router(Router(neighbor_cell))
 
     def random_move(self):
-        for i in range(0, random.randint(0, 4)):
+        for i in range(0, random.randint(0, 2)):
             random_router = random.choice(self.clone.routers)
             self.clone.remove_router(random_router)
             self.clone.add_router(Router(random.choice(Building.target_cells)))
 
     def add(self):
-        for i in range(0, random.randint(0, 4)):
+        for i in range(0, random.randint(0, 2)):
             added = self.clone.add_router(Router(random.choice(Building.target_cells)))
             if not added:
                 break
 
     def remove(self):
-        for i in range(0, random.randint(0, 4)):
+        for i in range(0, random.randint(0, 2)):
             self.clone.add_router(Router(random.choice(Building.target_cells)))
 
     def run(self):
